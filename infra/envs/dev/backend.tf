1abbfef72fceaa51996ddda147aac33795e64886
bt4it <bt4it2@gmail.com>
2026-08-26 00:15:16 +0200

    cleanning v301

---
+++ infra/envs/dev/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-15min-blinket-tfstate-gwc"
+    storage_account_name = "st15minblinkettfstate"
+    container_name       = "tfstate"
+    key                  = "dev/terraform.tfstate"
+
+    use_oidc = true
+  }
+}
