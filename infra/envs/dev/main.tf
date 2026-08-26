0e86706211a21dd844dc6d794e6edb6cd59df8fa
bt4it <bt4it2@gmail.com>
2026-08-26 13:46:07 +0200

    Preserve resource group tags; apply consistent tagging to new resources2

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -1,6 +1,12 @@
 resource "azurerm_resource_group" "dev" {
   name     = var.resource_group_name
   location = var.location
+
+  tags = {
+    project     = "15min-blinket"
+    environment = "dev"
+    managed-by  = "terraform"
+  }
 }
 
 resource "azurerm_log_analytics_workspace" "dev" {
@@ -23,17 +29,6 @@ resource "azurerm_container_app_environment" "dev" {
   resource_group_name        = azurerm_resource_group.dev.name
   logs_destination           = "log-analytics"
   log_analytics_workspace_id = azurerm_log_analytics_workspace.dev.id
-
-  tags = {
-    project     = "15min-blinket"
-    environment = "dev"
-    managed-by  = "terraform"
-  }
-}
-
-resource "azurerm_resource_group" "dev" {
-  name     = var.resource_group_name
-  location = var.location
 
   tags = {
     project     = "15min-blinket"
