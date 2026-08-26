cb96655802c80571091c68af11c3d859d9e41de1
bt4it <bt4it2@gmail.com>
2026-08-26 13:41:16 +0200

    Preserve resource group tags; apply consistent tagging to new resources

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -9,6 +9,12 @@ resource "azurerm_log_analytics_workspace" "dev" {
   resource_group_name = azurerm_resource_group.dev.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
+
+  tags = {
+    project     = "15min-blinket"
+    environment = "dev"
+    managed-by  = "terraform"
+  }
 }
 
 resource "azurerm_container_app_environment" "dev" {
@@ -17,4 +23,21 @@ resource "azurerm_container_app_environment" "dev" {
   resource_group_name        = azurerm_resource_group.dev.name
   logs_destination           = "log-analytics"
   log_analytics_workspace_id = azurerm_log_analytics_workspace.dev.id
+
+  tags = {
+    project     = "15min-blinket"
+    environment = "dev"
+    managed-by  = "terraform"
+  }
+}
+
+resource "azurerm_resource_group" "dev" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    project     = "15min-blinket"
+    environment = "dev"
+    managed-by  = "terraform"
+  }
 }
